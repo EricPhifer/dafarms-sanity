b731e12f50925ddc7655bd4ed19728720c936874
Eric Phifer <ericoncode@gmail.com>
2023-04-24 10:22:38 -0600

    update gitignore file

---
+++ dist/static/index-04cc5656-aff98313.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-b167003e-d3c01849.js";import"./sanity-6c2837ec.js";import"./index-2187cd5a.js";export{m as DocumentPaneProvider,a as default};
